9198d743aeeaeecc644ca436cc1ab25ffaf53059
Lukas Truemper <lukas.truemper@outlook.de>
2023-03-22 13:10:23 +0100

    Renamed module

--- src/sdfg_wrapper.cpp
+++ src/sdfg_wrapper.cpp
@@ -56,7 +56,7 @@ std::shared_ptr<sdfg_wrapper> sdfg_wrapper::create(std::string sdfg_path)
 {
     // py::scoped_interpreter guard{};
 
-    py::module_ daisy_backend = py::module_::import("daisy_backends.tiramisu");
+    py::module_ daisy_backend = py::module_::import("daisy_optimization.tiramisu");
     py::object py_result = daisy_backend.attr("convert")(sdfg_path);
 
     std::string repr_str = py_result.cast<std::string>();
